7bee28fa4867fbc04790b22b683cb0a29a930430
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-06 14:42:56 +0500

    Инициализация проекта

---
+++ src/main/java/com/ddkolesnik/avitotraiding/AvitoTraidingApplication.java
@@ -0,0 +1,13 @@
+package com.ddkolesnik.avitotraiding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvitoTraidingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvitoTraidingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddkolesnik/avitotraiding/AvitoTraidingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddkolesnik.avitotraiding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvitoTraidingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
