8324fd34398e9ef1baeaf6c3d4e3ed38b3c43971
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-13 20:07:14 +0500

    Добавлен общий класс, чтобы не писать похожий код

---
+++ src/main/java/com/ddkolesnik/avitotraiding/model/AbstractEntity.java
@@ -0,0 +1,39 @@
+package com.ddkolesnik.avitotraiding.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Getter
+@Setter
+@MappedSuperclass
+public class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "creation_time")
+    private LocalDateTime creationTime;
+
+    @Column(name = "modified_time")
+    private LocalDateTime modifiedTime;
+
+    @PrePersist
+    public void prePersist() {
+        this.creationTime = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.modifiedTime = LocalDateTime.now();
+    }
+
+}
