24bc8533a869a52bda2dfc9cb9667db669b8a78d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-13 19:45:52 +0500

    Добавлен общий интерфейс для сбора информации

---
+++ src/main/java/com/ddkolesnik/avitotraiding/repository/Grabber.java
@@ -0,0 +1,15 @@
+package com.ddkolesnik.avitotraiding.repository;
+
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+public interface Grabber {
+
+    Document getDocument(String url) throws IOException;
+
+}
