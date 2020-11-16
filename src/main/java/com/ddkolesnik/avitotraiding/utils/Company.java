c0ea644ebe0910f302646b55bec1109ee49f772e
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-16 11:42:09 +0500

    Добавлено перечисление для компаний

---
+++ src/main/java/com/ddkolesnik/avitotraiding/utils/Company.java
@@ -0,0 +1,29 @@
+package com.ddkolesnik.avitotraiding.utils;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+public enum Company {
+
+    SBER("Сбербанк", "sberbank_a"),
+    OPENING("Открытие", "i57915260"),
+    RT("Ростелеком", "realty_rostelecom");
+
+    private final String title;
+
+    private final String systemName;
+
+    Company(String title, String systemName) {
+        this.title = title;
+        this.systemName = systemName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSystemName() {
+        return systemName;
+    }
+}
