b54e155ddce10ca9521b79bf4c1a606d2769f4e7
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-16 10:56:30 +0500

    Расширен базовый класс. Добавлены поля.

--- src/main/java/com/ddkolesnik/avitotraiding/model/TradingEntity.java
+++ src/main/java/com/ddkolesnik/avitotraiding/model/TradingEntity.java
@@ -1,62 +1,82 @@
 package com.ddkolesnik.avitotraiding.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
 
 /**
  * @author Alexandr Stegnin
  */
 
 @Data
-public class TradingEntity {
+@EqualsAndHashCode(callSuper = true)
+public class TradingEntity extends AbstractEntity {
 
     /**
      * Лот
      */
+    @Column(name = "lot")
     private String lot;
 
     /**
      * Описание
      */
+    @Column(name = "description")
     private String description;
 
     /**
      * Адрес
      */
+    @Column(name = "address")
     private String address;
 
     /**
      * Номер торгов
      */
+    @Column(name = "trading_number")
     private String tradingNumber;
 
     /**
      * Идентификатор лота в ЕФРСБ
      */
+    @Column(name = "efrsb_id")
     private String efrsbId;
 
     /**
      * Шаг аукциона
      */
+    @Column(name = "auction_step")
     private String auctionStep;
 
     /**
      * Сумма задатка
      */
+    @Column(name = "deposit_amount")
     private String depositAmount;
 
     /**
      * Время проведения торгов
      */
+    @Column(name = "trading_time")
     private String tradingTime;
 
     /**
      * Период приёма заявок
      */
+    @Column(name = "accept_requests_date")
     private String acceptRequestsDate;
 
     /**
      * Ссылка на лот
      */
-    private String lotUrl;
+    @Column(name = "url")
+    private String url;
+
+    /**
+     * Стоимость
+     */
+    @Column(name = "cost")
+    private String cost;
 
 }
