e5d69cab6e87a8ec74f3fe0d09b9500381d4a875
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-18 19:15:45 +0500

    Добавлен идентификатор продавца.

--- src/main/java/com/ddkolesnik/avitotraiding/utils/Company.java
+++ src/main/java/com/ddkolesnik/avitotraiding/utils/Company.java
@@ -6,17 +6,20 @@ package com.ddkolesnik.avitotraiding.utils;
 
 public enum Company {
 
-    SBER("Сбербанк", "sberbank_a"),
-    OPENING("Открытие", "i57915260"),
-    RT("Ростелеком", "realty_rostelecom");
+    SBER("Сбербанк", "sberbank_a", "93c406fa3fdb2fb05c00b8c37233cad7"),
+    OPENING("Открытие", "i57915260", "59cfc27e2f6f83525a502de38b0e74b2"),
+    RT("Ростелеком", "realty_rostelecom", "8016f339f4fdc113fe58a485abf87702");
 
     private final String title;
 
     private final String systemName;
 
-    Company(String title, String systemName) {
+    private final String sellerId;
+
+    Company(String title, String systemName, String sellerId) {
         this.title = title;
         this.systemName = systemName;
+        this.sellerId = sellerId;
     }
 
     public String getTitle() {
@@ -25,6 +28,10 @@ public enum Company {
 
     public String getSystemName() {
         return systemName;
+    }
+
+    public String getSellerId() {
+        return sellerId;
     }
 
     public static Company fromSystemName(String systemName) {
