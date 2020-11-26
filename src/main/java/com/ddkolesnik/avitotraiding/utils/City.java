20e19db31cb5c50b886ac8044282e882d9360051
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-11-26 15:05:37 +0500

    Добавлен паттерн для отсеивания результата за пределами города.

--- src/main/java/com/ddkolesnik/avitotraiding/utils/City.java
+++ src/main/java/com/ddkolesnik/avitotraiding/utils/City.java
@@ -6,16 +6,19 @@ package com.ddkolesnik.avitotraiding.utils;
 
 public enum City {
 
-    TYUMEN("Тюмень", "%D0%A2%D1%8E%D0%BC%D0%B5%D0%BD%D1%8C"),
-    EKB("Екатеринбург", "%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3");
+    TYUMEN("Тюмень", "%D0%A2%D1%8E%D0%BC%D0%B5%D0%BD%D1%8C", "(тюменская\\s+обл)"),
+    EKB("Екатеринбург", "%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3", "(свердловская\\s+обл)");
 
     private final String name;
 
     private final String urlEncoded;
 
-    City(String name, String urlEncoded) {
+    private final String pattern;
+
+    City(String name, String urlEncoded, String pattern) {
         this.name = name;
         this.urlEncoded = urlEncoded;
+        this.pattern = pattern;
     }
 
     public String getName() {
@@ -24,5 +27,9 @@ public enum City {
 
     public String getUrlEncoded() {
         return urlEncoded;
+    }
+
+    public String getPattern() {
+        return pattern;
     }
 }
