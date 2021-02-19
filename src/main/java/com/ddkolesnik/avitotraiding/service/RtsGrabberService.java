b93a857ca4e6510a7ac6424a5050b1d9822f5c0a
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-02-19 09:42:01 +0500

    Доработан сервис для возможности сбора данных по торгам и по Екатеринбургу.

--- src/main/java/com/ddkolesnik/avitotraiding/service/RtsGrabberService.java
+++ src/main/java/com/ddkolesnik/avitotraiding/service/RtsGrabberService.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import com.ddkolesnik.avitotraiding.model.TradingEntity;
+import com.ddkolesnik.avitotraiding.utils.City;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 /**
  * Севрис для получения информации о торгах с площадки РТС
@@ -34,9 +34,9 @@ public class RtsGrabberService {
             "&procedureTypeNames=%D0%A0%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F%20%D0%B0%D1%80%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%BE%D0%B3%D0%BE%20%D0%B8%D0%BC%D1%83%D1%89%D0%B5%D1%81%D1%82%D0%B2%D0%B0" +
             "&procedureTypeNames=%D0%A0%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F%20%D0%B8%D0%BC%D1%83%D1%89%D0%B5%D1%81%D1%82%D0%B2%D0%B0,%20%D0%BE%D0%B1%D1%80%D0%B0%D1%89%D0%B5%D0%BD%D0%BD%D0%BE%D0%B3%D0%BE%20%D0%B2%20%D1%81%D0%BE%D0%B1%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D0%B3%D0%BE%D1%81%D1%83%D0%B4%D0%B0%D1%80%D1%81%D1%82%D0%B2%D0%B0" +
             "&procedureTypeNames=%D0%A0%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F%20(%D0%BF%D1%80%D0%BE%D0%B4%D0%B0%D0%B6%D0%B0)%20%D0%BD%D0%B5%D0%BF%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D1%85%20%D0%B8%20%D0%BF%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D1%85%20%D0%B0%D0%BA%D1%82%D0%B8%D0%B2%D0%BE%D0%B2" +
-            "&propertyAddress=%D0%A2%D1%8E%D0%BC%D0%B5%D0%BD%D1%81%D0%BA%D0%B0%D1%8F" +
             "&auctionStartDateTimeFrom=2021-01-01" +
-            "&tab=publicTrades";
+            "&tab=publicTrades" +
+            "&propertyAddress=";
 
     public RtsGrabberService(TradingService tradingService) {
         this.tradingService = tradingService;
@@ -47,8 +47,8 @@ public class RtsGrabberService {
      *
      * @return кол-во собранных лотов
      */
-    public int parse() {
-        List<TradingEntity> entities = getRtsLots();
+    public int parse(City city) {
+        List<TradingEntity> entities = getRtsLots(city);
         return getRtsLotInfo(entities);
     }
 
@@ -61,20 +61,23 @@ public class RtsGrabberService {
     private int getRtsLotInfo(List<TradingEntity> entities) {
         for (TradingEntity entity : entities) {
             waitFiltered(entity.getUrl(), "openPart");
-            String description = $(By.id("BaseMainContent_MainContent_ucTradeLotViewList_tlvLot_fvName_lblValue")).text();
+            SelenideElement description = $(By.id("BaseMainContent_MainContent_ucTradeLotViewList_tlvLot_hLotTitle"));
+            description.shouldBe(Condition.appear);
+            String descriptionText = description.text();
             String price = $(By.id("BaseMainContent_MainContent_ucTradeLotViewList_tlvLot_fvLotPrice_lblValue")).text();
             String address = $(By.id("BaseMainContent_MainContent_ucTradeLotViewList_tlvLot_fvDeliveryAddress_lblValue")).text();
             String auctionStep = $(By.id("BaseMainContent_MainContent_ucTradeLotViewList_tlvLot_fvAuctionStep_lblValue")).text();
             String depositAmount = $(By.id("BaseMainContent_MainContent_ucTradeLotViewList_tlvLot_fvEarnestSum_lblValue")).text();
             String seller = $(By.id("BaseMainContent_MainContent_fvSeller_lblValue")).text();
-            entity.setDescription(description);
-            entity.setPrice(BigDecimal.valueOf(Long.parseLong(price.split(",")[0])));
+            entity.setDescription(descriptionText);
+            entity.setPrice(BigDecimal.valueOf(Long.parseLong(price.split(",")[0].replaceAll("\\s", ""))));
             entity.setAddress(address);
             entity.setAuctionStep(auctionStep);
             entity.setDepositAmount(depositAmount);
             entity.setSeller(seller);
             tradingService.create(entity);
         }
+        closeWebDriver();
         return entities.size();
     }
 
@@ -83,8 +86,9 @@ public class RtsGrabberService {
      *
      * @return список лотов
      */
-    private List<TradingEntity> getRtsLots() {
-        waitFiltered(RTS_FILTERED_URL, "lots-table");
+    private List<TradingEntity> getRtsLots(City city) {
+        String url = RTS_FILTERED_URL + city.getRegion();
+        waitFiltered(url, "lots-table");
         SelenideElement lotsTable = $(By.cssSelector("div.lots-table"));
         List<SelenideElement> elements = lotsTable.$$(By.cssSelector("[data-type-id=proceduresListRow]"));
         String lotNumber = "";
@@ -98,6 +102,7 @@ public class RtsGrabberService {
             lotUrl = href.attr("href");
             if (lotUrl != null) {
                 lotNumber = lotUrl.split("#")[1];
+                lotUrl = lotUrl.replace("http://", "https://");
             }
             if (fields.size() > 10) {
                 acceptRequestsDate = fields.get(8).concat(" ").concat(fields.get(9));
@@ -106,6 +111,7 @@ public class RtsGrabberService {
                 tradingTime = fields.get(10).concat(" ").concat(fields.get(11));
             }
             TradingEntity tradingEntity = new TradingEntity();
+            tradingEntity.setCity(city.getName());
             tradingEntity.setLot(lotNumber);
             tradingEntity.setUrl(lotUrl);
             tradingEntity.setLotSource("РТС");
@@ -129,7 +135,7 @@ public class RtsGrabberService {
     private void waitFiltered(String url, String processName) {
         open(url);
         WebDriverRunner.getWebDriver().manage().window().fullscreen();
-        WebDriverRunner.getWebDriver().manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        WebDriverRunner.getWebDriver().manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
         SelenideElement process = $(By.className(processName));
         process.shouldBe(Condition.visible);
     }
